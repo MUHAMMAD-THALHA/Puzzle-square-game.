c8bd66c51db7824ec00a5d1d782cf0fccf0746b9
MUHAMMAD THALHA <144235908+MUHAMMAD-THALHA@users.noreply.github.com>
2023-12-10 20:51:26 +0530

    Add files via upload

---
+++ Puzzle.js
@@ -0,0 +1,275 @@
+// Game functioning code.
+var board;
+var score = 0;
+var rows = 4;
+var columns = 4;
+const left = document.getElementById("Left")
+const right = document.getElementById("Right")
+const up = document.getElementById("Up")
+const down = document.getElementById("Down")
+window.onload = function() {
+    setGame();
+}
+
+function setGame() {
+    // board = [
+    //     [2, 2, 2, 2],
+    //     [2, 2, 2, 2],
+    //     [4, 4, 8, 8],
+    //     [4, 4, 8, 8]
+    // ];
+
+    board = [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    ]
+
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < columns; c++) {
+            let tile = document.createElement("div");
+            tile.id = r.toString() + "-" + c.toString();
+            let num = board[r][c];
+            updateTile(tile, num);
+            document.getElementById("board").append(tile);
+        }
+    }
+    //create 2 to begin the game
+    setTwo();
+    setTwo();
+
+}
+
+// Functions for controlling buttons.
+function updateTile(tile, num) {
+    tile.innerText = "";
+    tile.classList.value = ""; //clear the classList
+    tile.classList.add("tile");
+    if (num > 0) {
+        tile.innerText = num.toString();
+        if (num <= 4096) {
+            tile.classList.add("x"+num.toString());
+        } else {
+            tile.classList.add("x8192");
+        }                
+    }
+}
+left.addEventListener("click",function(){
+    slideLeft();
+    setTwo();
+});
+right.addEventListener("click",function(){
+    slideRight();
+    setTwo();
+});
+up.addEventListener("click",function(){
+    slideUp();
+    setTwo();
+});
+down.addEventListener("click",function(){
+    slideDown();
+    setTwo();
+});
+
+// Controllers.
+document.addEventListener('keyup', (e) => {
+    if (e.code == "ArrowLeft") {
+        slideLeft();
+        setTwo();
+    }
+    else if (e.code == "ArrowRight") {
+        slideRight();
+        setTwo();
+    }
+    else if (e.code == "ArrowUp") {
+        slideUp();
+        setTwo();
+
+    }
+    else if (e.code == "ArrowDown") {
+        slideDown();
+        setTwo();
+    }
+    document.getElementById("score").innerHTML = score;
+    console.log("Scores:",score)
+    localStorage.setItem("score",score)
+})
+
+function filterZero(row){
+    return row.filter(num => num != 0); //create new array of all nums != 0
+}
+
+function slide(row) {
+    //[0, 2, 2, 2] 
+    row = filterZero(row); //[2, 2, 2]
+    for (let i = 0; i < row.length-1; i++){
+        if (row[i] == row[i+1]) {
+            row[i] *= 2;
+            row[i+1] = 0;
+            score += row[i];
+        }
+    } //[4, 0, 2]
+    row = filterZero(row); //[4, 2]
+    //add zeroes
+    while (row.length < columns) {
+        row.push(0);
+    } //[4, 2, 0, 0]
+    return row;
+}
+
+//Slide left to score. 
+console.log("score",score)
+function slideLeft() {
+    for (let r = 0; r < rows; r++) {
+        let row = board[r];
+        row = slide(row);
+        board[r] = row;
+        for (let c = 0; c < columns; c++){
+            let tile = document.getElementById(r.toString() + "-" + c.toString());
+            let num = board[r][c];
+            updateTile(tile, num);
+        }
+    }
+}
+
+//Slide Right to score.
+function slideRight() {
+    for (let r = 0; r < rows; r++) {
+        let row = board[r];         //[0, 2, 2, 2]
+        row.reverse();              //[2, 2, 2, 0]
+        row = slide(row)            //[4, 2, 0, 0]
+        board[r] = row.reverse();   //[0, 0, 2, 4];
+        for (let c = 0; c < columns; c++){
+            let tile = document.getElementById(r.toString() + "-" + c.toString());
+            let num = board[r][c];
+            updateTile(tile, num);
+        }
+    }
+}
+//Slide Up to score.
+function slideUp() {
+    for (let c = 0; c < columns; c++) {
+        let row = [board[0][c], board[1][c], board[2][c], board[3][c]];
+        row = slide(row);
+        // board[0][c] = row[0];
+        // board[1][c] = row[1];
+        // board[2][c] = row[2];
+        // board[3][c] = row[3];
+        for (let r = 0; r < rows; r++){
+            board[r][c] = row[r];
+            let tile = document.getElementById(r.toString() + "-" + c.toString());
+            let num = board[r][c];
+            updateTile(tile, num);
+        }
+    }
+}
+
+//Slide Down to score.
+function slideDown() {
+    for (let c = 0; c < columns; c++) {
+        let row = [board[0][c], board[1][c], board[2][c], board[3][c]];
+        row.reverse();
+        row = slide(row);
+        row.reverse();
+        // board[0][c] = row[0];
+        // board[1][c] = row[1];
+        // board[2][c] = row[2];
+        // board[3][c] = row[3];
+        for (let r = 0; r < rows; r++){
+            board[r][c] = row[r];
+            let tile = document.getElementById(r.toString() + "-" + c.toString());
+            let num = board[r][c];
+            updateTile(tile, num);
+        }
+    }
+}
+
+// Redirecting to the gameover page after the tiles blocked.
+function setTwo() {
+    if (!hasEmptyTile()) {
+        location.href="./gameover.html"
+        return;
+    }
+    let found = false;
+    while (!found) {
+        //find random row and column to place a 2 in
+        let r = Math.floor(Math.random() * rows);
+        let c = Math.floor(Math.random() * columns);
+        if (board[r][c] == 0) {
+            board[r][c] = 2;
+            let tile = document.getElementById(r.toString() + "-" + c.toString());
+            tile.innerText = "2";
+            tile.classList.add("x2");
+            found = true;
+        }
+    }
+}
+
+function hasEmptyTile() {
+    let count = 0;
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < columns; c++) {
+            if (board[r][c] == 0) { //at least one zero in the board
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+
+// Audio Controllers.
+// Audio button
+const gameAudio = document.getElementById("gameAudio");
+const audioToggleBtn = document.getElementById("audioToggle");
+
+// Function to play the audio
+const playAudio = () => {
+  gameAudio.play();
+};
+
+// Function to pause the audio
+const pauseAudio = () => {
+  gameAudio.pause();
+};
+
+// Function to toggle play/pause
+const toggleAudio = () => {
+  if (gameAudio.paused) {
+    playAudio();
+  } else {
+    pauseAudio();
+  }
+};
+
+// Event listener for when the audio ends, restart it
+gameAudio.addEventListener("ended", () => {
+  playAudio();
+});
+
+// Event listener for the button to toggle audio
+audioToggleBtn.addEventListener("click", () => {
+  toggleAudio();
+  // Save the audio state to sessionStorage
+  sessionStorage.setItem("audioState", gameAudio.paused ? "off" : "on");
+});
+
+// Event listener for when the page is unloaded 
+window.addEventListener("beforeunload", () => {
+  // Save the audio playback position to sessionStorage
+  sessionStorage.setItem("audioPlaybackPosition", gameAudio.currentTime);
+});
+
+// Start playing the audio when the page loads
+if (sessionStorage.getItem("audioState") === "on") {
+  playAudio();
+}
+
+// Restore the audio playback position from sessionStorage
+const savedPosition = sessionStorage.getItem("audioPlaybackPosition");
+if (savedPosition) {
+  gameAudio.currentTime = parseFloat(savedPosition);
+}
+
+// Audio code ends...//
